39f8bce5281012addba18929726c93bf475a32c3
huuuunee <s21024@gsm.hs.kr>
2023-06-20 18:19:22 +0900

    :sparkles: Jwt 관련 에러를 잡는 Filter 생성

---
+++ src/main/kotlin/com/graduate/cheese/global/security/filter/JwtTokenFilter.kt
@@ -0,0 +1,28 @@
+package com.graduate.cheese.global.security.filter
+
+import com.graduate.cheese.global.security.jwt.JwtTokenProvider
+import org.slf4j.LoggerFactory
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtTokenFilter(
+    private val jwtTokenProvider: JwtTokenProvider
+) : OncePerRequestFilter() {
+    private val log = LoggerFactory.getLogger(this::class.simpleName)
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token: String? = jwtTokenProvider.resolveToken(request)
+        if (!token.isNullOrBlank()) {
+            val authentication = jwtTokenProvider.authentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+            log.info("current user email = ${authentication.name}")
+        }
+        filterChain.doFilter(request, response)
+    }
+}
