e25c59cc8f47d8f82b9c5f5859d50023c9a03349
huuuunee <s21024@gsm.hs.kr>
2023-06-20 18:14:10 +0900

    :sparkles: AuthDetailsService 생성

---
+++ src/main/kotlin/com/graduate/cheese/global/security/auth/AuthDetailsService.kt
@@ -0,0 +1,19 @@
+package com.graduate.cheese.global.security.auth
+
+import com.graduate.cheese.domain.user.exception.UserNotFoundException
+import com.graduate.cheese.domain.user.domain.repository.UserRepository
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class AuthDetailsService(
+    private val userRepository: UserRepository
+) : UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userRepository.findByEmail(username) ?: throw UserNotFoundException
+        return AuthDetails(user)
+    }
+}
