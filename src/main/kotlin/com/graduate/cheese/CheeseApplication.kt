499e249bc588c97b43d84c20f077b1c059d7d2f7
huuuunee <s21024@gsm.hs.kr>
2023-06-20 18:07:52 +0900

    :sparkles: Application 에 Configuration 추가

--- src/main/kotlin/com/graduate/cheese/CheeseApplication.kt
+++ src/main/kotlin/com/graduate/cheese/CheeseApplication.kt
@@ -1,9 +1,11 @@
 package com.graduate.cheese
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class CheeseApplication
 
 fun main(args: Array<String>) {
