564bbe286f861f8dac1cad1bad4115aeab778e73
huuuunee <s21024@gsm.hs.kr>
2023-06-20 18:22:37 +0900

    :sparkles: User 엔티티 생성

---
+++ src/main/kotlin/com/graduate/cheese/domain/user/domain/entity/User.kt
@@ -0,0 +1,30 @@
+package com.graduate.cheese.domain.user.domain.entity
+
+import com.graduate.cheese.domain.auth.domain.Role
+import org.hibernate.annotations.GenericGenerator
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "user")
+class User (
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(columnDefinition = "BINARY(16)")
+    var id: UUID,
+
+    @Column(length = 20)
+    val email: String,
+
+    @Column
+    val nickName: String,
+
+    @Column
+    val password: String,
+
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "Role", joinColumns = [JoinColumn(name = "user_id")])
+    var roles: MutableList<Role> = mutableListOf(),
+    )
