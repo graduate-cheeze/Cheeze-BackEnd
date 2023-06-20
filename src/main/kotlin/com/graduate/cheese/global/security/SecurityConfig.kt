2c6f46ea44b0404cb33a349f3ea74437db0be871
huuuunee <s21024@gsm.hs.kr>
2023-06-20 18:20:30 +0900

    :sparkles: SecurityConfig 생성 및 주소 추가

---
+++ src/main/kotlin/com/graduate/cheese/global/security/SecurityConfig.kt
@@ -0,0 +1,55 @@
+package com.graduate.cheese.global.security
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.graduate.cheese.global.config.FilterConfig
+import com.graduate.cheese.global.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.RequestMatcher
+import org.springframework.web.cors.CorsUtils
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper
+) {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
+            .httpBasic().disable()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .authorizeRequests()
+            .requestMatchers(RequestMatcher { request ->
+                CorsUtils.isPreFlightRequest(request)
+            }).permitAll()
+
+            .antMatchers(HttpMethod.POST, "/auth").permitAll()
+            .antMatchers(HttpMethod.POST, "/auth/signin").permitAll()
+
+            .anyRequest().denyAll()
+            .and()
+            .exceptionHandling()
+            .authenticationEntryPoint(CustomAuthenticationEntryPoint(objectMapper))
+
+            .and()
+            .apply(FilterConfig(jwtTokenProvider, objectMapper))
+            .and()
+            .build()
+    }
+
+    @Bean
+    protected fun passwordEncoder() = BCryptPasswordEncoder()
+}
