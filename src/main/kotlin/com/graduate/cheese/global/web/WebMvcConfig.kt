2ed1f2e5cc1622c38baa0c69006d4b880a0607d6
huuuunee <s21024@gsm.hs.kr>
2023-06-26 18:21:28 +0900

    :sparkles: cors 설정 추가

---
+++ src/main/kotlin/com/graduate/cheese/global/web/WebMvcConfig.kt
@@ -0,0 +1,16 @@
+package com.graduate.cheese.global.web
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebMvcConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8080")
+            .allowedHeaders("*")
+            .allowedMethods("*")
+            .allowCredentials(true)
+    }
+}
