0f5271a229060ea066f9c0ada01b4af186b96b39
huuuunee <s21024@gsm.hs.kr>
2023-06-26 18:20:20 +0900

    :sparkles: 이미지 업로드 후 반환할때 필요한 Dto 생성

---
+++ src/main/kotlin/com/graduate/cheese/domain/image/presentation/dto/data/ImageListResponseData.kt
@@ -0,0 +1,5 @@
+package com.graduate.cheese.domain.image.presentation.dto.data
+
+data class ImageListResponseData(
+    val imageUrl: List<String>
+)
---
+++ src/main/kotlin/com/graduate/cheese/domain/image/presentation/dto/res/ImageListResponseDto.kt
@@ -0,0 +1,5 @@
+package com.graduate.cheese.domain.image.presentation.dto.res
+
+data class ImageListResponseDto(
+    val imageUrl: List<String>
+)
