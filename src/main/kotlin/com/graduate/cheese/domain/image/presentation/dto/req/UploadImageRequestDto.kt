3c04913362be11d79ca3e6480dd0af6a2ff199e5
huuuunee <s21024@gsm.hs.kr>
2023-06-30 11:00:15 +0900

    :sparkles: 필요없는거 삭제

--- src/main/kotlin/com/graduate/cheese/domain/image/presentation/dto/req/UploadImageRequestDto.kt
+++
@@ -1,7 +0,0 @@
-package com.graduate.cheese.domain.image.presentation.dto.req
-
-import org.springframework.web.multipart.MultipartFile
-
-data class UploadImageRequestDto(
-    val images: List<MultipartFile>
-)
